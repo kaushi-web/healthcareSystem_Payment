45403a5483066059b9cbc7a0dfa760f9d3793a9c
kaushi-web <itsmekaush1sk@gmail.com>
2020-04-17 00:23:31 +0530

    edit readAllPymentRecorda function

--- src/com/healthcareSystem_Payment/service/payment/paymentService.java
+++ src/com/healthcareSystem_Payment/service/payment/paymentService.java
@@ -21,27 +21,31 @@ public class paymentService {
 	
 	
 	public String StoreOnlinePayment(Payment payment) {
-		String query1 = " INSERT INTO `payment`(`Payment_method`, `paid_time`, `purpose`, `amount`, `status`) VALUES (?,?,?,?,?)";
+		String query1 = " INSERT INTO `payment`(`user_id`,`hospital_id`,`Payment_method`,`appointment_id`,`doctor_id`, `purpose`, `amount`, `status`) VALUES (?.?,?,?,?,?,?,?)";
 		
 		String query2 = " INSERT INTO `card_details`(`payment_id`, `card_type`, `card_number`, `cvv`, `expiryDate`) VALUES (?,?,?,?,?)";
 		PreparedStatement preparedStmt1;
 		PreparedStatement preparedStmt2;
-		DateFormat df = new SimpleDateFormat(" yyyy-mm-dd hh:mm:ss");
-	       Date dateobj = new Date();
-	       System.out.println(df.format(dateobj));
-	       Timestamp ts=Timestamp.valueOf(df.format(dateobj));
-	       payment.setPaymenttime(ts);
+		/*
+		 * DateFormat df = new SimpleDateFormat(" yyyy-mm-dd hh:mm:ss"); Date dateobj =
+		 * new Date(); System.out.println(df.format(dateobj)); Timestamp
+		 * ts=Timestamp.valueOf(df.format(dateobj)); payment.setPaymenttime(ts);
+		 */
 		String output;
 		try {
 			preparedStmt1 = db.connection.prepareStatement(query1);
-			preparedStmt1.setString(1, payment.getPaymentmethod());
-			preparedStmt1.setTimestamp(2,payment.getPaymenttime());
-			preparedStmt1.setString(3,payment.getPurpose());
-			preparedStmt1.setFloat(3,payment.getAmount());
-			preparedStmt1.setString(4,payment.getStatues());
+			
+			preparedStmt1.setInt(1, payment.getUserid());
+			preparedStmt1.setInt(2, payment.getHospitalid());
+			preparedStmt1.setString(3, payment.getPaymentmethod());
+			preparedStmt1.setInt(4, payment.getAppointmentid());
+			preparedStmt1.setInt(5, payment.getDoctorid());
+			preparedStmt1.setString(6,payment.getPurpose());
+			preparedStmt1.setFloat(7,payment.getAmount());
+			preparedStmt1.setString(8,payment.getStatues());
 			preparedStmt1.execute();
 			 
-			 if(payment.getPaymentmethod()=="cash") {
+			 if(payment.getPaymentmethod()=="Card") {
 			preparedStmt2 = db.connection.prepareStatement(query2);
 			preparedStmt2.setInt(1,1);
 			preparedStmt2.setString(2,payment.getCardtype());
@@ -62,21 +66,20 @@ public class paymentService {
 		return output;
 	}
 	
-	public String readAllPymentRecords() {
+public String readAllPymentRecords() {
 		
 		String output="";
 		// Prepare the html table to be displayed
-					output = "<table border=\"1\"><tr><th>User ID</th><th>Payment ID</th><th>Paymment Method</th><th>Payment time</th><th>Purpose</th><th>Amount</th><th>Statues</th></tr>";
+					output = "<table border=\"1\"><tr><th>Payment ID</th><th>Appointment ID</th><th>Paymment Method</th><th>Payment time</th><th>Purpose</th><th>Amount</th><th>Statues</th></tr>";
 					String query = "select * from `payment`";
 					try {
 					
-					//	Statement stmt = db.connection.createStatement();
 						Statement stmt = db.connection.createStatement();
 						ResultSet rs=stmt.executeQuery(query);
 						// iterate through the rows in the result set
 						while (rs.next()) {
-							String userId = Integer.toString(rs.getInt("user_id"));
 							String paymentid = Integer.toString(rs.getInt("payment_id"));
+							String appointmentid = Integer.toString(rs.getInt("appointment_id"));
 							String paymentMethod = rs.getString("Payment_method");
 							DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
 							String paymentTime =dateFormat.format(rs.getDate("paid_time")); 	
@@ -86,7 +89,7 @@ public class paymentService {
 							
 							// Add into the html table
 							output += "<tr><td>" + paymentid + "</td>";
-							output += "<td>" + userId + "</td>";
+							output += "<td>" + appointmentid + "</td>";
 							output += "<td>" + paymentMethod + "</td>";
 							output += "<td>" + paymentTime + "</td>";
 							output += "<td>" + purpose + "</td>";
