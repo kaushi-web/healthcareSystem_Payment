95bd9283e190c68cede530bce101571698f840a8
kaushi-web <itsmekaush1sk@gmail.com>
2020-05-03 13:59:05 +0530

    modify null validations in payment method

--- WebContent/Component/payment.js
+++ WebContent/Component/payment.js
@@ -1,59 +1,56 @@
 // page refresh moment
 $(document).ready(function() {
-	if($("#alertSuccess").text().trim()=="")
-		{
+	if ($("#alertSuccess").text().trim() == "") {
 		$("#alertSuccess").hide();
-		}
+	}
 	$("#alertError").hide();
 });
 
 // add event handler to save button
-$(document).on("click", "#btnSave", function(event)
-{
+$(document).on("click", "#btnSave", function(event) {
 	// Clear status msges---------------------
 	$("#alertSuccess").text("");
 	$("#alertSuccess").hide();
 	$("#alertError").text("");
 	$("#alertError").hide();
 
+	// Form validation-------------------
+	var status = validateItemForm();
 
-// Form validation-------------------
-var status = validateItemForm();
-
-// If not valid
-if (status != true) {
-	$("#alertError").text(status);
-	$("#alertError").show();
-	return;
-}
-//if valid
-$("#formPayment").submit();
+	// If not valid
+	if (status != true) {
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	}
+	// if valid
+	$("#formPayment").submit();
 
 });
-function validateItemForm(){
-	
-	//user ID
-	if ($("#userid").val().trim() == "")
-	 {
-	 return "Insert User ID.";
-	 }
-	
-	// Appointment ID
-	if ($("#appointmentid").val().trim() == "")
-	 {
-	 return "Insert Appointment ID.";
-	 }
-	
-	//Purpose
-	if ($("#purpose").val().trim() == "")
-	 {
-	 return "Insert purpose.";
-	 }
+function validateItemForm() {
 
-	//Amount
-	if ($("#amount").val().trim() == "")
-	 {
-	 return "Insert amount.";
-	 }
+	// user ID
+	if ($("#userid").val().trim() == "") {
+		return "Insert User ID.";
+	}
+
+	// Appointment ID
+	if ($("#appointmentid").val().trim() == "") {
+		return "Insert Appointment ID.";
+	}
+
+	// Payment Method
+	if ($('input[name="radiopaymentmethod"]:checked').length === 0) {
+		return "Select payment method.";
+	}
+	// Purpose
+	if ($("#purpose").val().trim() == "") {
+		return "Insert purpose.";
+	}
+
+	// Amount
+	if ($("#amount").val().trim() == "") {
+		return "Insert amount.";
+	}
 	return true;
 }
